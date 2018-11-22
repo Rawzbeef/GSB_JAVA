c4e973f31f71f808c60f32de468081e62edc84c1
Awonefr <wonealexis@hotmail.fr>
2018-11-22 11:59:18 +0100

    Update VueAfficherFrais.java

--- src/fr/gsb/vue/VueAfficherFrais.java
+++ src/fr/gsb/vue/VueAfficherFrais.java
@@ -3,6 +3,11 @@ package fr.gsb.vue;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.ItemSelectable;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 import javax.swing.*;
 /*
@@ -14,20 +19,48 @@ import utiliser:
  */
 @SuppressWarnings("serial")
 
-public class VueAfficherFrais extends JPanel {
+public class VueAfficherFrais extends JPanel implements ItemListener{
+	//JComboBox
 	private JComboBox listeVisiteur;
-	private JPanel North;
-	private JPanel middle;
-	private JPanel down;
-
+	private JComboBox listeMois;
+	//JPanel
+	private JPanel JPNorth;
+	private JPanel JPdown;
+	
+	// mettre private ArrayList<Visiteur> LesVisiteurs
+	
 	public VueAfficherFrais(){
 		this.setLayout(new BorderLayout());
-		North = new JPanel();
+		JPNorth = new JPanel();
+		
+		//listeVisiteur
 		listeVisiteur = new JComboBox();
 		String[] tab = {"Option 1", "Option 2", "Option 3", "…"};
 		listeVisiteur = new JComboBox(tab);
-		listeVisiteur.setPreferredSize(new Dimension(100,20));
-		North.add(this.listeVisiteur, BorderLayout.NORTH);
+		
+		ItemListener itemListener = new ItemListener() {
+		      public void itemStateChanged(ItemEvent itemEvent) {
+		        System.out.println("Item: " + itemEvent.getItem());
+		      }
+		    };
+		    listeVisiteur.addItemListener(itemListener);
+		    listeVisiteur.setPreferredSize(new Dimension(100,20));
+			JPNorth.add(this.listeVisiteur);
+		
+		//listMois
+		listeMois = new JComboBox();
+		String[] tab2 = {"Option 1", "Option 2", "Option 3", "…"};
+		listeMois = new JComboBox(tab);
+		listeMois.setPreferredSize(new Dimension(100,20));
+		JPNorth.add(this.listeMois);
+		
+		
+		//ajout dans le PanelPrinciapl
+		this.add(JPNorth, BorderLayout.NORTH);
+	}
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
 }
