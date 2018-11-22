06b10e7e8c1f1e447cec84c295ccc37c99bab8ca
Rawzbeef <vincent.gaspar@hotmail.fr>
2018-11-22 08:21:41 +0100

    Create VueMenu.java

---
+++ src/fr/gsb/vue/VueMenu.java
@@ -0,0 +1,17 @@
+package fr.gsb.vue;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+import fr.gsb.objet.Gsb;
+
+@SuppressWarnings("serial")
+public class VueMenu extends JMenuBar {
+
+	
+	public VueMenu(Gsb gsb, Vue vue, VueMessage vueMessage) {
+		
+	}
+
+}
