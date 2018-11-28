1d5f4da28d4feabf5a00c27354633a4e1b147119
Awonefr <wonealexis@hotmail.fr>
2018-11-28 17:11:17 +0100

    Update Gsb.java

--- src/fr/gsb/objet/Gsb.java
+++ src/fr/gsb/objet/Gsb.java
@@ -1,8 +1,12 @@
 package fr.gsb.objet;
 
+import java.util.ArrayList;
+
+import fr.gsb.modele.ModeleBDD;
+
 public class Gsb {
-	
+	private ArrayList<String> lesVisiteur;
 	public Gsb() {
-		
+		this.lesVisiteur=ModeleBDD.initLesVisiteur();
 	}
 }
