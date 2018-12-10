d41912ba07c3190e93e927b640b82fcbe146a142
Awonefr <wonealexis@hotmail.fr>
2018-12-10 12:16:55 +0100

    Update VuePDF.java

--- src/fr/gsb/vue/VuePDF.java
+++ src/fr/gsb/vue/VuePDF.java
@@ -1,5 +1,43 @@
 package fr.gsb.vue;
+import java.io.File;
+import java.io.IOException; 
+
+import org.apache.pdfbox.pdmodel.*;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
 public class VuePDF {
-	//https://www.tutorialspoint.com/pdfbox/pdfbox_adding_text.htm
+
+	public static void main (String args[]) throws IOException {
+		File file = new File("P:/pdfTest/test.pdf"); 
+		PDDocument document = PDDocument.load(file); 
+		PDPage page = document.getPage(0);
+		PDPageContentStream contentStream = new PDPageContentStream(document, page);
+
+		System.out.println("PDF loaded"); 
+		contentStream.beginText(); 
+
+		//Setting the font to the Content stream  
+		contentStream.setFont(PDType1Font.TIMES_ROMAN, 12);
+
+		//Setting the position for the line 
+		contentStream.newLineAtOffset(25, 500);
+
+		String text = "This is the sample document and we are adding content to it.";
+
+		//Adding text in the form of string 
+		contentStream.showText(text);      
+
+		//Ending the content stream
+		contentStream.endText();
+
+		System.out.println("Content added");
+
+		//Closing the content stream
+		contentStream.close();
+
+		//Saving the document
+		document.save(new File("P:/pdfTest/test.pdf")); 
+
+
+	}  
 }
